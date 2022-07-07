efb0762823ecd417f4ca41e0caee095a6a84e99c
B.Orgilbat <zettry1@gmail.com>
2022-07-07 10:04:26 -0500

    feat: lab11 update

--- lab11.js
+++ lab11.js
@@ -8,17 +8,33 @@ dns.lookup("www.miu.edu", (_err, address) => {
 //question 2
 
 const http = require("http");
+const fs = require("fs");
 
 const hostname = "127.0.0.1";
 const port = 3000;
+const server = http.createServer();
 
-const server = http.createServer((req, res) => {
+server.on("request", (req, res) => {
   console.log("server", req);
   res.statusCode = 200;
   res.setHeader("Content-Type", "");
   const pic = fs.readFileSync(path.join(__dirname, "./happy.jpeg"));
   res.data.pipe(pic);
 });
+
+//sol 2
+server
+  .on("request", (req, res) => {
+    const img = require("fs").createReadStream("./tiles.jpg");
+    img.pipe(res);
+  })
+  .listen(9090, () => console.log("listening to 9090"));
+
+// solution3
+server.on("request", (req, res) => {
+  fs.createReadStream("./tiles.jpg").pipe(res);
+});
+server.listen(9000);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
